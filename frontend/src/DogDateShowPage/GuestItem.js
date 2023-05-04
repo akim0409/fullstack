8b72c21f7b61248b6246cef6d49027839ffe3c80
Autumn Kim <autumnkim0409@gmail.com>
2023-05-03 21:51:17 -0400

    Add image Error handle on DogDateShowPage/GuestItem

--- frontend/src/DogDateShowPage/GuestItem.js
+++ frontend/src/DogDateShowPage/GuestItem.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 const GuestItem = (props) => {
   const { guest, onClick, border } = props;
+  const [isValidUrl, setIsValidUrl] = useState(true);
   return (
     <div 
       className="flex flex-col items-center mx-4 my-8 hover:cursor-pointer"
       onClick={onClick}
     >
-      <img
+      {
+        isValidUrl ? <img
         className={`w-[160px] h-[160px] object-cover rounded-full shadow-lg shadow-stone-400 ${border ? 'border-[4px] border-yellow-500' : ''}`}
         alt="dog"
         src={guest.imageUrl}
-      />
+        onError={() => {
+          setIsValidUrl(false);
+        }}
+      /> :  <div className={`w-[160px] h-[160px] rounded-full flex justify-center items-center shadow-lg shadow-stone-400 text-orange-800 bg-orange-400/80 ${border ? 'border-[4px] border-yellow-500' : ''}`}>
+      <div className="flex flex-col items-center">
+        <i className="text-5xl fa-solid fa-bone-break mb-2"></i>
+        <div className="text-xs">dog image not found</div>
+      </div>
+    </div>
+      }
+      
       <div className="mt-4 flex flex-col items-center text-zinc-600">
         <div className="flex items-center relative">
           <div className="font-ubuntu text-3xl">{guest.name}</div>
